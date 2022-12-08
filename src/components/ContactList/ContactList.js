22114899369a8744258ca1af0c670e08137f0538
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-12-09 01:59:02 +0200

    summary

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
+import ContactItem from '../ContactItem/ContactItem';
+import style from './ContactList.module.css';
+import { deleteContact } from '../../Redux/phonebook/phonebook-actions';
+import { getVisible } from '../../Redux/phonebook/phonebook-selectors';
+
+export default function ContactList() {
+  const contacts = useSelector(getVisible);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <ul className={style.list}>
+        {contacts.map(contact => {
+          return (
+            <ContactItem
+              key={contact.id}
+              contact={contact}
+              delet={id => dispatch(deleteContact(id))}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+import App from './components/App/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.querySelector('#root')
 );
